d964c0a9c8c6c49ce46e3f321f0f2ff2a8f2bc9e
Adalbertjunior <adalbertjunior@souunilavras.com>
2025-04-12 16:08:15 -0300

    teste azure

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "Hello Azure!";
 ?> 
+<link rel="shortcut icon" href="https://www.php.net/images/logos/php-med-trans.png" type="image/x-icon">
 <h1> Teste Plus</h1>'
 <h2>teste plus 2</h2>
 <h3>Teste Plus 3</h3>
