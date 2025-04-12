390295559298a41f495a8111979c57196f62114c
Adalbertjunior <adalbertjunior@souunilavras.com>
2025-04-12 16:10:59 -0300

    teste azure

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hello Azure!";
 ?> 
-<link rel="shortcut icon" href="https://www.php.net/images/logos/php-med-trans.png" type="image/x-icon">
 <h1> Teste Plus</h1>'
 <h2>teste plus 2</h2>
 <h3>Teste Plus 3</h3>
+<img src="https://unilavras.edu.br/new_site/wp-content/uploads/2020/06/IMG_114559.jpg" alt="unilavras">
